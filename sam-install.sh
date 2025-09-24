a6d9a87e9d3ab4ba1af5ef7f14c82cd229f7cf8f
seelism <122780082+seelism@users.noreply.github.com>
2025-09-24 15:29:10 +0800

    Update sam-install.sh

--- sam-install.sh
+++ sam-install.sh
@@ -9,6 +9,11 @@ curl -L "https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli
 unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
 sudo ./sam-installation/install
 
+echo "Installing AWS CLI"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 echo "Installing docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
