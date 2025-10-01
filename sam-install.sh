8fef4bd52cdc712fb6ce4f5d99de502b777a9381
seelism <122780082+seelism@users.noreply.github.com>
2025-10-01 21:36:15 +0800

    Update sam-install.sh

--- sam-install.sh
+++ sam-install.sh
@@ -18,6 +18,10 @@ echo "Installing docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
 echo "Installation finished"
 sam --version
 docker --version
